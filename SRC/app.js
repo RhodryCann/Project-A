561f4e72dfeaf2a3b1fe4480237639702cc02fe4
RhodryCann <85023762+RhodryCann@users.noreply.github.com>
2023-01-23 14:38:47 +0000

    minor changes - configured prettier / added eslint

--- SRC/app.js
+++ SRC/app.js
@@ -1,53 +1,38 @@
 //moving onto JSx & npm + prettier (for autoformatting)
- 
+
 const Pet = (props) => {
-    //add object props
-    //one way data flow from App into Pet through props
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.species),
-        React.createElement("h2", {}, props.breed),
-        React.createElement("h2", {}, props.colour)
-    ])
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.species),
+    React.createElement("h2", {}, props.breed),
+    React.createElement("h2", {}, props.colour),
+  ]);
 };
 
-
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "adopt me"), // add Pet attributes
-            React.createElement(Pet, {
-                name: "Chewy",
-                species: "Cat",
-                breed: "Maine Coon",
-                colour: "Ginger"
-            }),
-            React.createElement(Pet, {
-                name: "Luna",
-                species: "Dog",
-                breed: "Havenese",
-                colour: "Black"
-            }),
-            React.createElement(Pet, {
-                name: "Tutti",
-                species: "Cat",
-                breed: "Short Hair",
-                colour: "Grey"
-            })
-        ]
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "adopt me"), // add Pet attributes
+    React.createElement(Pet, {
+      name: "Chewy",
+      species: "Cat",
+      breed: "Maine Coon",
+      colour: "Ginger",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      species: "Dog",
+      breed: "Havenese",
+      colour: "Black",
+    }),
+    React.createElement(Pet, {
+      name: "Tutti",
+      species: "Cat",
+      breed: "Short Hair",
+      colour: "Grey",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App)); //render "root" *createElement passed component "App" instead of string
-                                        //could have had attributes/children like above but not needed here
-
-// React is good for creating components.. made up of other components for reusable code (macro components made up from micro components) 
-// It's a small library to learn for rendering ui, great at handling lots of state that generates interfaces from state
-
-// JSx will use the createElement function for you
-
-
+root.render(React.createElement(App));
