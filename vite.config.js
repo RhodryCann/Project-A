b528ad511a61404f8cef90d92a661520316c3691
RhodryCann <85023762+RhodryCann@users.noreply.github.com>
2023-01-26 16:40:21 +0000

    installed node latest v18

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   //uses src folder to crawl structure and understand files html/js/css etc
   plugins: [react()],
-  root: "SRC",
+  root: "src",
 });
